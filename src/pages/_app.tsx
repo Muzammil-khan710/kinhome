f1528273ea244c73a940087ce1042428e55a82d3
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-03-11 12:14:00 +0530

    added web vitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,17 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { onCLS, onFCP, onLCP } from 'web-vitals'
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  useEffect(() => {
+    onCLS(console.log)
+    onFCP(console.log)
+    onLCP(console.log)
+  }, [])
+
   return (
     <>
       <Head>
